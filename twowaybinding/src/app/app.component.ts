efef7eb1d33320c9b81dfce0023b35408970907d
Birendra Mahto <divyabiru27@gmail.com>
2025-03-01 23:17:37 +0530

    updated the code

--- twowaybinding/src/app/app.component.ts
+++ twowaybinding/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'twowaybinding';
+  title = 'What is two way binding';
+  name: string = 'Ravi';
 }
+
+/*
+-- what is two way binding
+--make input field for two way binding
+--import formModudle
+-- apply ngModel
+
+
+*/
