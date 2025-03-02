a9705550e872f09a5f01e662086b6e8ec3ccdc1e
Birendra Mahto <divyabiru27@gmail.com>
2025-03-02 14:26:14 +0530

    todo completed

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo App in Angular';
+  task: string = '';
+  taskList: { id: number; title: string }[] = [];
+
+  addNewTaks() {
+    this.task &&
+      this.taskList.push({
+        title: this.task,
+        id: (this.taskList[this.taskList.length - 1]?.id || 0) + 1,
+      });
+    console.log('&&&', this.taskList[this.taskList.length - 1].id);
+    this.task = '';
+  }
+  inputHandle(event: Event) {
+    this.task = (event.target as HTMLInputElement).value;
+  }
+
+  onDelete(id: number) {
+    console.log(id, '888888888888', this.taskList);
+    this.taskList = this.taskList.filter((task) => id != task.id);
+  }
 }
